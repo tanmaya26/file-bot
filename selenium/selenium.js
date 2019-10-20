e34503b95219778d6d37e1c62105d31f4da0d606
Tanmaya Nanda <tnanda@ncsu.edu>
2019-10-20 13:33:25 -0400

    only add bot functionality

--- selenium/selenium.js
+++ selenium/selenium.js
@@ -24,19 +24,6 @@ function logout(driver, url) {
     driver.findElement(By.xpath("/html/body/div[6]/div/div/div/div/div/div/div[13]/button/div")).click();
 }
 
-async function UseCaseUploadPicture(driver, url) {
-
-    await driver.get(url);
-    await driver.findElement(By.name("email")).sendKeys(loginEmail);
-    await driver.findElement(By.name("password")).sendKeys(loginPassword, Key.RETURN);
-    await driver.findElement(By.css('.c-button-unstyled.p-message_input_file_button[data-qa="msg_input_file_btn"]')).click();
-    file_input = driver.findElement(By.css('.c-button-unstyled.c-menu_item__button[data-qa="add_file_from_computer_menu_item"]'));
-    await file_input.sendKeys("file1.png");
-    await driver.findElement(By.xpath("/html/body/div[6]/div/div/div[3]/div[2]/button")).click();
-    await driver.sleep(2000);
-    // await assert();
-}
-
 async function UseCaseSendStorageWarning(driver, url) {
 
     await driver.findElement(By.xpath("/html/body/div[2]/div/div/div[4]/div/div/footer/div/div/div[1]/div/div[1]")).sendKeys("@fileninja --setStorageSize 3", Key.RETURN);
