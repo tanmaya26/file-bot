0da4c49194810160ea1362bbfaa73582fcce6939
Tanmaya Nanda <tnanda@ncsu.edu>
2019-10-21 09:49:01 -0400

    more test cases

--- selenium/selenium.js
+++ selenium/selenium.js
@@ -26,17 +26,26 @@ async function logout(driver, url) {
 
 async function askFilebot(driver) {
     await driver.sleep(2000);
-    await driver.findElement(By.xpath("//a[@href='https://csc510workspace.slack.com/messages/CNK1N4V5F']")).click();
+    await driver.findElement(By.xpath("/html/body/div[2]/div/div/div[3]/div/nav/div[2]/div[1]/div/div[1]/div/div/div[11]/a/span")).click();
 }
 
-async function UseCaseSendStorageWarningWithParams(driver) {
+async function UseCaseSetStorageLimitWithParams(driver) {
     await driver.findElement(By.xpath("/html/body/div[2]/div/div/div[4]/div/div/footer/div/div/div[1]/div/div[1]")).sendKeys("@fileninja --setStorageSize 3", Key.RETURN);
-
     await driver.sleep(3000);
+    driver.findElements(By.className("c-message__body")).then(function (elements) {
+        elements[elements.length - 1].getText().then(function (text) {
+            try {
+                assert.equal("Size Limit has been set to 3", text);
+                console.log('Usecase 2: "Set storage limit" expectedly passed.');
+            } catch (e) {
+                console.log('Usecase 2: "Set storage limit" unexpectedly failed when parameter given');
+                return Promise.resolve('Usecase to set storage warning failed.');
+            }
+        })
+    });
 }
 
-async function UseCaseSendStorageWarningWithoutParams(driver) {
-
+async function UseCaseSetStorageLimitWithoutParams(driver) {
     await driver.findElement(By.xpath("/html/body/div[2]/div/div/div[4]/div/div/footer/div/div/div[1]/div/div[1]")).sendKeys("@fileninja --setStorageSize", Key.RETURN);
     await driver.sleep(2000);
 
@@ -44,14 +53,81 @@ async function UseCaseSendStorageWarningWithoutParams(driver) {
         elements[elements.length - 1].getText().then(function (text) {
             try {
                 assert.equal("Not a number", text);
-                console.log('Usecase 2: Expectedly failed when wrong/no parameter given');
+                console.log('Usecase 2: "Set storage limit" expectedly failed when wrong/no parameter given');
             } catch (e) {
-                console.log('Usecase 2: Unexpectedly passed when wrong/no parameter given');
+                console.log('Usecase 2: "Set storage limit" unexpectedly failed when wrong/no parameter given');
                 return Promise.resolve('Usecase to set storage warning failed.');
             }
         })
     });
+}
 
+async function UseCaseGetStorage(driver) {
+    await driver.findElement(By.xpath("/html/body/div[2]/div/div/div[4]/div/div/footer/div/div/div[1]/div/div[1]")).sendKeys("@fileninja --getStorageSize", Key.RETURN);
+    await driver.sleep(2000);
+
+    driver.findElements(By.className("c-message__body")).then(function (elements) {
+        elements[elements.length - 1].getText().then(function (text) {
+            try {
+                assert.equal("Current size limit is 3", text);
+                console.log('Usecase 3: "Get storage limit" expectedly passed.');
+            } catch (e) {
+                console.log('Usecase 3: "Get storage limit" unexpectedly failed.');
+                return Promise.resolve('Usecase to get storage limit failed.');
+            }
+        })
+    });
+}
+
+async function UseCaseRegisterCategoryWithParams(driver) {
+
+    await driver.findElement(By.xpath("/html/body/div[2]/div/div/div[4]/div/div/footer/div/div/div[1]/div/div[1]")).sendKeys("@fileninja --registerCategory Project1", Key.RETURN);
+    await driver.sleep(2000);
+
+    driver.findElements(By.className("c-message__body")).then(function (elements) {
+        elements[elements.length - 1].getText().then(function (text) {
+            try {
+                assert.equal("Category has been added.", text);
+                console.log('Usecase 4.1: "Register category" expectedly passed.');
+            } catch (e) {
+                console.log('Usecase 4.1: "Register category" unexpectedly failed.');
+                return Promise.resolve('Usecase to register category failed.');
+            }
+        })
+    });
+
+    await driver.findElement(By.xpath("/html/body/div[2]/div/div/div[4]/div/div/footer/div/div/div[1]/div/div[1]")).sendKeys("@fileninja --registerCategory Project1", Key.RETURN);
+    await driver.sleep(2000);
+
+    driver.findElements(By.className("c-message__body")).then(function (elements) {
+        elements[elements.length - 1].getText().then(function (text) {
+            try {
+                assert.equal("Error. Category name already exists.", text);
+                console.log('Usecase 4.2: "Register category" expectedly failed when user tried to add same category again.');
+            } catch (e) {
+                console.log('Usecase 4.2: Unexpectedly failed.');
+                return Promise.resolve('Usecase to register category failed.');
+            }
+        })
+    });
+}
+
+async function UseCaseRegisterCategoryWithoutParams(driver) {
+
+    await driver.findElement(By.xpath("/html/body/div[2]/div/div/div[4]/div/div/footer/div/div/div[1]/div/div[1]")).sendKeys("@fileninja --registerCategory", Key.RETURN);
+    await driver.sleep(2000);
+
+    driver.findElements(By.className("c-message__body")).then(function (elements) {
+        elements[elements.length - 1].getText().then(function (text) {
+            try {
+                assert.equal("Not a number", text);
+                console.log('Usecase 5: Expectedly failed when wrong/no parameter given');
+            } catch (e) {
+                console.log('Usecase 5: Unexpectedly failed when wrong/no parameter given');
+                return Promise.resolve('Usecase to register category failed.');
+            }
+        })
+    });
 }
 
 (async () => {
@@ -59,7 +135,10 @@ async function UseCaseSendStorageWarningWithoutParams(driver) {
     let driver = await new Builder().forBrowser("chrome").build();
     await login(driver, slackUrl);
     await askFilebot(driver);
-    await UseCaseSendStorageWarningWithParams(driver);
-    await UseCaseSendStorageWarningWithoutParams(driver);
+    await UseCaseSetStorageLimitWithParams(driver);
+    await UseCaseSetStorageLimitWithoutParams(driver);
+    await UseCaseGetStorage(driver);
+    await UseCaseRegisterCategoryWithParams(driver);
+    //await UseCaseRegisterCategoryWithoutParams(driver);
     //await logout(driver, "https://csc510workspace.slack.com");
 })()
