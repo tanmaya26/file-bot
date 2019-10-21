5e01c3c1c2b872bee72a830084ed679dc9df1226
Tanmaya Nanda <tnanda@ncsu.edu>
2019-10-20 20:47:13 -0400

    fixes

--- selenium/selenium.js
+++ selenium/selenium.js
@@ -12,30 +12,35 @@ async function example() {
     await driver.findElement(By.name("domain")).sendKeys("csc510workspace", Key.RETURN);
 }
 
-function login(driver, url) {
-    driver.get(url);
-    driver.findElement(By.name("email")).sendKeys(loginEmail);
-    driver.findElement(By.name("password")).sendKeys(loginPassword, Key.RETURN);
-    driver.sleep(2000);
+async function login(driver, url) {
+    await driver.get(url);
+    await driver.findElement(By.name("email")).sendKeys(loginEmail);
+    await driver.findElement(By.name("password")).sendKeys(loginPassword,Key.RETURN);
 }
 
-function logout(driver, url) {
-    driver.findElement(By.className("c-icon p-classic_nav__team_header__team__icon c-icon--chevron-down")).click();
-    driver.findElement(By.xpath("/html/body/div[6]/div/div/div/div/div/div/div[13]/button/div")).click();
+async function logout(driver, url) {
+    await driver.findElement(By.className("c-icon p-classic_nav__team_header__team__icon c-icon--chevron-down")).click();
+    await driver.findElement(By.xpath("/html/body/div[6]/div/div/div/div/div/div/div[13]/button/div")).click();
 }
 
-async function UseCaseSendStorageWarning(driver, url) {
-
-    await driver.findElement(By.xpath("/html/body/div[2]/div/div/div[4]/div/div/footer/div/div/div[1]/div/div[1]")).sendKeys("@fileninja --setStorageSize 3", Key.RETURN);
+async function askFilebot(driver) {
     await driver.sleep(2000);
+
+    await driver.findElement(By.xpath("//a[@href='https://csc510workspace.slack.com/messages/DNE0ELZNX']")).click();
+}
+
+async function UseCaseSendStorageWarning(driver) {
+    
+    await driver.findElement(By.xpath("/html/body/div[2]/div/div/div[4]/div/div/footer/div/div/div[1]/div/div[1]")).sendKeys("@fileninja --setStorageSize 3", Key.RETURN);
+    await driver.sleep(1000);
 
 }
 
 (async () => {
 
     let driver = await new Builder().forBrowser("chrome").build();
-    await login(driver, "https://csc510workspace.slack.com");
-    await UseCaseSendStorageWarning(driver, "https://csc510workspace.slack.com");
-    await logout(driver, "https://csc510workspace.slack.com");
-    driver.quit();
+    await login(driver, slackUrl);
+    await askFilebot(driver);
+    await UseCaseSendStorageWarning(driver);
+    //await logout(driver, "https://csc510workspace.slack.com");
 })()
