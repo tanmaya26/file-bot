095a60c097e9fb3db9810293a2692fb2c3cfaac8
Tanmaya Nanda <tnanda@ncsu.edu>
2019-10-25 23:02:06 -0400

    add files to category

--- src/dbservice/category_dbservice.js
+++ src/dbservice/category_dbservice.js
@@ -20,7 +20,8 @@ async function create(obj) {
                 console.error("Unable to add item. Error JSON:", JSON.stringify(err, null, 2));
             } else {
                 console.log("Added item:", JSON.stringify(data, null, 2));
-                resolve(true);
+                var res = "Category registered.";
+                resolve(res);
             }
         });
     });
@@ -29,12 +30,12 @@ async function create(obj) {
 async function get_all(channel_name) {
     var docClient = new AWS.DynamoDB.DocumentClient();
     var params = {
-        TableName: table,
+        TableName: "category",
         IndexName: "ChannelIndex",
         KeyConditionExpression: "channel = :a",
         ExpressionAttributeValues: {
-        ":a": channel_name
-    }
+            ":a": channel_name
+        }
     };
     return await new Promise((resolve, reject) => {
         docClient.query(params, function (err, data) {
@@ -49,5 +50,31 @@ async function get_all(channel_name) {
     });
 }
 
+async function add_file(files, channel_name, category_name) {
+    var docClient = new AWS.DynamoDB.DocumentClient();
+    var params = {
+        TableName: "files",
+        Item: {
+            "channel_name": channel_name,
+            "category_name": category_name,
+            "files": files
+        }
+    };
+    return await new Promise((resolve, reject) => {
+        docClient.put(params, function (err, data) {
+            if (err) {
+                console.error("Unable to add item. Error JSON:", JSON.stringify(err, null, 2));
+            } else {
+                console.log("Added item:", JSON.stringify(data, null, 2));
+                var res = "File added to category.";
+                resolve(res);
+            }
+        });
+    });
+}
+
+
+
 module.exports.create = create;
 module.exports.get_all = get_all;
+module.exports.add_file = add_file;
--- src/service/slack_bot_service.js
+++ src/service/slack_bot_service.js
@@ -12,6 +12,7 @@ var bot = new slackBot({
 });
 
 var bot_id = process.env.BOT_ID
+var file_url = "https://slack.com/api/files.list";
 
 async function get_slack_resource_from_url(url) {
 	const resource = await fetch(url, {
@@ -30,13 +31,36 @@ async function get_slack_resource_from_url(url) {
 async function get_json_data_from_url(url) {
 	const resource = await fetch(url, {
 		method: 'GET'
-	}).then(function(response){
+	}).then(function (response) {
 		return response.json();
 	})
-	.then(function(json){
-		return json
-	});
+		.then(function (json) {
+			return json
+		});
 	return resource
+}
+
+async function get_file_from_slack(channel, file_name) {
+	var url = new URL(file_url);
+	params = { channel: channel };
+	url.search = new URLSearchParams(params).toString();
+	const resource = await fetch(url, {
+		method: 'GET',
+		mode: 'cors',
+		withCredentials: true,
+		credentials: 'include',
+		headers: {
+			'Content-Type': 'application/json',
+			'Authorization': 'Bearer ' + process.env.BEARER_TOKEN
+		}
+	}).then((res) => res.json())
+	var files = resource.files.filter(function (item) {
+		return item.name == file_name;
+	})
+		.map(function (item) {
+			return item.id;
+		});
+	return files
 }
 
 module.exports.slack = slack;
@@ -44,3 +68,4 @@ module.exports.bot = bot;
 module.exports.bot_id = bot_id;
 module.exports.get_slack_resource_from_url = get_slack_resource_from_url;
 module.exports.get_json_data_from_url = get_json_data_from_url;
+module.exports.get_file_from_slack = get_file_from_slack;
