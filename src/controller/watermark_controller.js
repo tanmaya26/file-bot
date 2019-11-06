5e714ff22b25b9d5832b62ecf3c79f5a8e11ee68
ayush.arnav <ayush.arnav@gmail.com>
2019-11-05 22:04:37 -0500

    Add text watermark feature + refactoring

--- src/controller/watermark_controller.js
+++ src/controller/watermark_controller.js
@@ -7,9 +7,6 @@ var fs = require('fs');
 var PDFLib = require('pdf-lib');
 var Jimp = require('jimp');
 var PDFDocument = PDFLib.PDFDocument;
-var rgb = PDFLib.rgb;
-var degrees = PDFLib.degrees;
-var StandardFonts = PDFLib.StandardFonts;
 var bot = slack_bot_service.bot
 
 async function init(command, data) {
@@ -39,6 +36,15 @@ async function init(command, data) {
 		}
 
 	}
+	else if (command[1] == 'text') {
+		if (command.length == 3) {
+			watermark_files(data, command)
+		}
+		else {
+			// Provide a text for this watermark
+			bot_response = "Please Provide a text for this watermark"
+		}
+	}
 	else if (command[1] == 'list') {
 		bot_response = await watermark_service.get_all_watermarks(data.channel)
 	}
@@ -47,7 +53,7 @@ async function init(command, data) {
 			bot_response = "No file associated with command. Upload a PDF file with command watermark the file."
 		}
 		else {
-			watermark_files(data, command[1])
+			watermark_files(data, command)
 		}
 	}
 
@@ -55,11 +61,10 @@ async function init(command, data) {
 	return bot_response
 }
 
-function watermark_files(data, watermark_name) {
+function watermark_files(data, command) {
 	if (watermark_service.check_if_all_pdf(data)) {
 		data.files.forEach(f => {
-			//image_watermark_PDF(f.url_private, watermark_name, data.channel).then((water_marked_file) => {
-				text_watermark_Pdf2(f.url_private, "Yo yo Ayubouy").then((water_marked_file) => {
+			watermark(f.url_private, command, data).then((water_marked_file) => {
 				fs.writeFile("temp_" + f.name, water_marked_file, function (err, result) {
 					file_upload_service.upload_file_via_bot("temp_" + f.name, "watermark_" + f.name, data.channel)
 						.then((res) => {
@@ -72,29 +77,39 @@ function watermark_files(data, watermark_name) {
 		});
 	}
 }
-async function image_watermark_PDF(pdf_url, watermark_name, channel_name) {
-	const url = pdf_url
-	pdfBytes = {}
-	const existingPdfBytes = await slack_bot_service.get_slack_resource_from_url(url)
 
-	const pdfDoc = await PDFDocument.load(existingPdfBytes).catch(function (error) {
+function watermark(url, command, data) {
+	if (command[1] == 'text') {
+		return text_watermark_Pdf(url, command[2])
+	}
+	else {
+		return image_watermark_PDF(url, command[1], data.channel)
+	}
+}
+
+async function image_watermark_PDF(pdf_url, watermark_name, channel_name) {
+	var url = pdf_url
+	pdfBytes = {}
+	var existingPdfBytes = await slack_bot_service.get_slack_resource_from_url(url)
+
+	var pdfDoc = await PDFDocument.load(existingPdfBytes).catch(function (error) {
 		console.log(error);
 	});
-	const pngUrl = await watermark_service.get_watermark(watermark_name, channel_name).then((res) => res)
-	const pngImageBytes = await slack_bot_service.get_slack_resource_from_url(pngUrl)
+	var pngUrl = await watermark_service.get_watermark(watermark_name, channel_name).then((res) => res)
+	var pngImageBytes = await slack_bot_service.get_slack_resource_from_url(pngUrl)
 
 	await Jimp.read(pngImageBytes)
 		.then(async (image) => {
 			await image.opacity(0.1).getBufferAsync(Jimp.MIME_PNG).then(async (im) => {
-				const pngImage = await pdfDoc.embedPng(im)
-				const pngDims = pngImage.scale(0.5)
+				var pngImage = await pdfDoc.embedPng(im)
+				var pngDims = pngImage.scale(0.5)
 
-				const pages = pdfDoc.getPages()
+				var pages = pdfDoc.getPages()
 				pages.forEach(page => {
 
 					page.drawImage(pngImage, {
 						x: page.getWidth() / 2 - pngDims.width / 2,
-						y: page.getHeight() / 2 - pngDims.height / 2 + 250,
+						y: page.getHeight() / 2 - pngDims.height / 2,
 						width: pngDims.width,
 						height: pngDims.height
 					})
@@ -106,51 +121,33 @@ async function image_watermark_PDF(pdf_url, watermark_name, channel_name) {
 }
 
 async function text_watermark_Pdf(pdf_url, watermark_text) {
-	const url = pdf_url
-	const existingPdfBytes = await slack_bot_service.get_slack_resource_from_url(url)
+	var url = pdf_url
+	pdfBytes = {}
+	var existingPdfBytes = await slack_bot_service.get_slack_resource_from_url(url)
 
-	const pdfDoc = await PDFDocument.load(existingPdfBytes).catch(function (error) {
+	var pdfDoc = await PDFDocument.load(existingPdfBytes).catch(function (error) {
 		console.log(error);
 	});
-	const helveticaFont = await pdfDoc.embedFont(StandardFonts.Helvetica)
-	const pages = pdfDoc.getPages()
-	const firstPage = pages[0]
-	const { width, height } = firstPage.getSize()
+	var pages = pdfDoc.getPages()
 
-	firstPage.drawText(watermark_text, {
-		x: width / 2,
-		y: height / 2 + 300,
-		size: 50,
-		font: helveticaFont,
-		color: rgb(0.95, 0.1, 0.1),
-		rotate: degrees(-45),
-	})
-
-	const pdfBytes = await pdfDoc.save()
-	return pdfBytes;
-}
-
-async function text_watermark_Pdf2(pdf_url, watermark_text) {
-	var image = new Jimp(300, 530, 'green', (err, image) => {
+	var image = new Jimp(pages[0].getWidth(), pages[0].getHeight(), (err, image) => {
 		if (err) throw err
 	})
 
-	let message = watermark_text
-	let x = 10
-	let y = 10
+	var message = watermark_text
+	var x = pages[0].getWidth() / 2 - (watermark_text.length * pages[0].getWidth() / 36)
+	var y = pages[0].getHeight() / 2
 
 	await Jimp.loadFont(Jimp.FONT_SANS_64_BLACK)
-		.then(font => {
-			await image.print(font, x, y, message).opacity(0.1).getBufferAsync(Jimp.MIME_PNG).then(async (im) => {
-				const pngImage = await pdfDoc.embedPng(im)
-				const pngDims = pngImage.scale(0.5)
+		.then(async (font) => {
+			await image.print(font, x, y, message).opacity(0.3).getBufferAsync(Jimp.MIME_PNG).then(async (im) => {
+				var pngImage = await pdfDoc.embedPng(im)
+				var pngDims = pngImage.scale(1)
 
-				const pages = pdfDoc.getPages()
 				pages.forEach(page => {
-
 					page.drawImage(pngImage, {
 						x: page.getWidth() / 2 - pngDims.width / 2,
-						y: page.getHeight() / 2 - pngDims.height / 2 + 250,
+						y: page.getHeight() / 2 - pngDims.height / 2,
 						width: pngDims.width,
 						height: pngDims.height
 					})
@@ -158,6 +155,7 @@ async function text_watermark_Pdf2(pdf_url, watermark_text) {
 				pdfBytes = await pdfDoc.save()
 			});
 		})
+	return pdfBytes;
 }
 
 module.exports.init = init;
