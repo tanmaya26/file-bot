d240eb325a35286459dc8adc2b5c0305be8f0f8a
ke2ulpatani <ke2ul.patani@gmail.com>
2019-12-01 09:51:10 -0500

    optimization

---
+++ src/CI-CD/jenkins.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+WORKSPACE=$1
+
+pwd
+ls
+whoami
+
+cd src
+cp $HOME/.env $WORKSPACE/.env
+if [ $? -ne 0 ]
+then
+  echo "Copying failed"
+  exit 1
+fi
+npm install --suppress-package-metadata-warnings
+if [ $? -ne 0 ]
+then
+  echo "npm install failed"
+  exit 1
+fi
+npm test test/watermark.js
+if [ $? -ne 0 ]
+then
+  echo "npm test failed"
+fi
+npm test test/export.js
+if [ $? -ne 0 ]
+then
+  echo "npm test failed"
+fi
+npm test test/category.js
+if [ $? -ne 0 ]
+then
+  echo "npm test failed"
+fi
